42d184bb20f14fabaade8b9ca90b6111e0cf8419
RobBro24 <79070744+RobBro24@users.noreply.github.com>
2025-04-07 15:47:26 -0400

    DetailsPage Revised

--- ContactsAppMVVM/Models/ViewModels/ContatcsDetailsPageViewModel.cs
+++ ContactsAppMVVM/Models/ViewModels/ContatcsDetailsPageViewModel.cs
@@ -13,5 +13,14 @@ namespace ContactsAppMVVM.Models.ViewModels
         [ObservableProperty]
         private Contact? contact;
 
+        [RelayCommand]
+        private void BackToAdd()
+        {
+            if (App.Current.MainPage is NavigationPage navigationPage)
+            {
+                // Assuming you want to navigate to a new page, you need to pass a Page object to PushAsync
+                navigationPage.Navigation.PushAsync(new MainPage());
+            }
+        }
     }
 }
